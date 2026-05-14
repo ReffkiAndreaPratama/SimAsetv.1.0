7dc309fe4108f28fc2a9a8a8d999b9097c1a9248
reffki <147146843+ReffkiAndreaPratama@users.noreply.github.com>
2026-05-14 19:58:46 +0700

    chore: add vercel config

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+// Set working directory ke root project
+$root = dirname(__DIR__);
+chdir($root);
+
+// Load autoloader
+require $root . '/vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once $root . '/bootstrap/app.php';
+
+// Handle request
+$app->handleRequest(Illuminate\Http\Request::capture());
